30c8f0ee5947b32f4b4c5ec1ead80e1672f45f40
Rafael Ye <122421657+Rafael3497@users.noreply.github.com>
2023-11-17 02:04:51 -0300

    Subir os arquivos do meu site

---
+++ links.js
@@ -0,0 +1,46 @@
+
+
+ScrollReveal().reveal('.subtitle', 
+{ duration: 1000,
+  rotate:{
+    x:0,
+    y:80,
+    z:0,
+  }
+})
+
+ScrollReveal().reveal('.card', 
+{ duration: 1100,
+    rotate:{
+        x:0,
+        y:80,
+        z:0,
+      }
+})
+
+ScrollReveal().reveal('.container3 p', 
+{ duration: 600,
+    rotate:{
+        x:0,
+        y:80,
+        z:0,
+      }
+})
+
+ScrollReveal().reveal('.mySwiper', 
+{ duration: 1500,
+    rotate:{
+        x:100,
+        y:0,
+        z:0,
+      }
+})
+
+ScrollReveal().reveal('.container3 h1', 
+{ duration: 700,
+    rotate:{
+        x:100,
+        y:0,
+        z:0,
+      }
+})
